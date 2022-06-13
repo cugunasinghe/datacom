25b02214ead0e6859ee903b694b7823a3278b0f3
cugunasinghe <chamodgunasinghe93@gmail.com>
2022-06-14 01:59:17 +0530

    Add files via upload
    Added API Manager call, which will be responsible for handling the API calls
    Added appSettings.json file

---
+++ Timesheet Processor/ApiManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Timesheet_Processor
+{
+    /// <summary>
+    /// this manager class will be used to handle api calls
+    /// </summary>
+    public class ApiManager
+    {
+        string _baseUrl, _clientId, _clientSeecret;
+
+        public ApiManager(IConfiguration configuration)
+        {
+            _baseUrl = configuration.GetSection("baseUrl").Value;
+            _clientId = configuration.GetSection("clienId").Value;
+            _clientSeecret = configuration.GetSection("clientSecret").Value;
+        }
+    }
+}
--- Timesheet Processor/Program.cs
+++ Timesheet Processor/Program.cs
@@ -1,23 +1,26 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 using Timesheet_Processor.Models;
 
 namespace Timesheet_Processor
 {
     class Program
     {
+        private static ApiManager _apiManager;
+
         static void Main(string[] args)
         {
             // display application welcome message
             Console.WriteLine("--------------------------------------------\n");
-            Console.WriteLine("            TIMESHEEET PROCESSOR            \r");
+            Console.WriteLine("            TIMESHEET PROCESSOR             \r");
             Console.WriteLine("--------------------------------------------\n");
 
             // requesting user for the company code, start date and the end date
             UserRequest.ReadUserInput();
 
-            Console.WriteLine(UserRequest.GetCompanyCode());
-            Console.WriteLine(UserRequest.GetPayPeriodStartDate());
-            Console.WriteLine(UserRequest.GetPayPeriodEndDate());
+            //create api manger instance
+            _apiManager = new ApiManager(new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build());
         }
     }
 }
