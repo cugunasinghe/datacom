eaa0cc7475c37c0b839a762e230d43410396258c
cugunasinghe <chamodgunasinghe93@gmail.com>
2022-06-13 20:05:18 +0530

    Add files via upload
    Initial Commit

---
+++ Timesheet Processor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Timesheet_Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
